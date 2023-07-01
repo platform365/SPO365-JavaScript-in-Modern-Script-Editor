bfe27870c4213f086fe98aad6cbb3790e1abe9f5
Platform 365 <138092846+platform365@users.noreply.github.com>
2023-07-01 08:31:58 +0700

    add .ajax sample

--- 01 Add file JavaScript and CSS to SharePoint Sitepages.js
+++ 01 Add file JavaScript and CSS to SharePoint Sitepages.js
@@ -35,24 +35,49 @@ function get_File_JavaScript_module(url){
 }
 
 /*
-// Bootrap 4.6.2
 // JQuery 3.5.1
-var js1 = 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js';
-var js2 = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js';
+var js = 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js';
 
-var css1 = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css';
+get_File_JavaScript(js);
 
-
-get_File_JavaScript(js1);
-get_File_JavaScript(js2);
-
-get_File_CSS(css1);
+// show version of Jquery
+$().jquery;
 
 // clear "Content"
 $('article').html('');
 
-var js3 = 'https://unpkg.com/@fluentui/web-components@2.5.15/dist/web-components.min.js';
+// AJAX to SharePoint Online
+var url = 'https://platform365.sharepoint.com';
+var requestHeaders = {
+            "accept": "application/json;odata=verbose"
+//          , "content-type": "application/json;odata=verbose"
+};
 
-get_File_JavaScript_module(js3);
+var params = {};
+var param = JSON.stringify(params);
+
+$.ajax({
+    url: url
+    , method: "GET"
+    , headers: requestHeaders
+    , "content-type": "application/json;odata=verbose"
+    //, data: param
+    , datatype: "json"
+    , success: function (result){
+        console.log(result);
+    }
+    , async: false
+});
+
+// Bootrap 4.6.2
+var js = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js';
+var css = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css';
+
+get_File_JavaScript(js);
+get_File_CSS(css);
+
+var js = 'https://unpkg.com/@fluentui/web-components@2.5.15/dist/web-components.min.js';
+
+get_File_JavaScript_module(js);
 
 */
